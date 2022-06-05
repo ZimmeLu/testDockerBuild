7f244c07e5060c86d044f0ada228fcbf3149b465
harveyjustin7 <92264933+harveyjustin7@users.noreply.github.com>
2022-06-05 12:32:10 +0200

    commit justin

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express(); 
 app.get('/', (req, res) => {
-res.send('devops presentation on Monday 24.05.2022');
+res.send('devops fertigstellung Lernjournal on Sunday 05.06.2022');
 });
 
 app.listen(PORT, HOST); 
